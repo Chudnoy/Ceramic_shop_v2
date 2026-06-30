bfa9578cf2454773a5329dc6e6155185aaf9e6f7
Chudnoy <tanzhor@gmail.com>
2026-06-30 15:24:06 +0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask, session, render_template, request, redirect, url_for, flash
-from db import (init_db, get_db_connection, get_reviews_by_product, add_review_db, get_all_products, get_product_by_id, 
-                get_products_by_ids, product_exists, get_order_by_id, get_all_orders, create_order, get_all_categories, 
-                get_category_by_slug, get_product_with_category, get_products_by_category)
-from validation import validate_review
-from services.cart_service import get_cart, add_to_cart_serv, remove_from_cart_serv, clear_cart
-import uuid
+from flask import Flask, session, 
+from db import init_db
 from routes.admin import admin_bp
 from routes.main import main_bp
 from datetime import timedelta
