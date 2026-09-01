01d3d36ca46ebd1661139a9fb8d959ff3be26813
Chudnoy <tanzhor@gmail.com>
2026-09-01 14:29:05 +0300

    Исправил test_new_public_home_opens

--- tests/test_public_routes.py
+++ tests/test_public_routes.py
@@ -2,4 +2,4 @@ def test_new_public_home_opens(client):
     response = client.get("/v2/")
 
     assert response.status_code == 200
-    assert "Новый Ceramic Shop" in response.get_data(as_text=True)
+    assert "Полина Яланская" in response.get_data(as_text=True)
