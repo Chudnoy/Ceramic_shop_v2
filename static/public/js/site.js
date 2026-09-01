3a2a88d9f9efa000a36f22b4044a95a6443688e4
Chudnoy <tanzhor@gmail.com>
2026-09-01 14:25:45 +0300

    Собрал desktop-версию новой публичной главной

--- static/public/js/site.js
+++ static/public/js/site.js
@@ -1,10 +1,7 @@
-const navigation = document.querySelector(".site-nav");
-const toggle = document.querySelector(".site-nav__toggle");
-
+document.documentElement.classList.add("js");
+const navigation = document.querySelector("[data-nav]");
+const toggle = navigation?.querySelector(".home-nav__toggle");
 if (navigation && toggle) {
-    toggle.addEventListener("click", () => {
-        const isOpen = navigation.classList.toggle("is-open");
-
-        toggle.setAttribute("aria-expanded", String(isOpen));
-    });
+  toggle.addEventListener("click", () => { const open = navigation.classList.toggle("is-open"); toggle.setAttribute("aria-expanded", String(open)); });
+  navigation.querySelectorAll("a").forEach((link) => link.addEventListener("click", () => { navigation.classList.remove("is-open"); toggle.setAttribute("aria-expanded", "false"); }));
 }
